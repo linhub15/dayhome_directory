d4b21561791077daea0cb7b35e1e7a98558c6271
Hubert Lin <linhub15@users.noreply.github.com>
2025-09-25 22:37:55 -0600

    fix: include crossorigin integrity check

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -40,6 +40,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       {
         src: "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
         integrity: "sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=",
+        crossOrigin: "",
       },
     ],
   }),
