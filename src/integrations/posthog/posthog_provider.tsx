63ac385bf88ffa0b98e03570ba79bb01a2f89851
Hubert Lin <linhub15@users.noreply.github.com>
2025-10-09 00:41:19 -0600

    fix: analytics try to use original implementation again

--- src/integrations/posthog/posthog_provider.tsx
+++ src/integrations/posthog/posthog_provider.tsx
@@ -1,18 +1,15 @@
-import posthog from "posthog-js";
 import { clientEnv } from "@/config/client_env";
 import { PostHogProvider } from "posthog-js/react";
-import { useEffect } from "react";
 
 export function Provider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    posthog.init(clientEnv.VITE_PUBLIC_POSTHOG_KEY, {
-      api_host: clientEnv.VITE_PUBLIC_POSTHOG_HOST,
-      defaults: "2025-05-24",
-    });
-  }, []);
-
   return (
-    <PostHogProvider client={posthog}>
+    <PostHogProvider
+      apiKey={clientEnv.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: clientEnv.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: "2025-05-24",
+      }}
+    >
       {children}
     </PostHogProvider>
   );
