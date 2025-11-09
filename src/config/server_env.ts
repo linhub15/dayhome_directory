5640b360a20c2c4fa36dbb4f63f423a377a02d28
Hubert Lin <linhub15@users.noreply.github.com>
2025-11-08 21:24:40 -0700

    chore: make geocoding optional

--- src/config/server_env.ts
+++ src/config/server_env.ts
@@ -2,7 +2,7 @@ import { z } from "@zod/zod";
 
 const envSchema = z.object({
   DATABASE_URL: z.url(),
-  MAPBOX_TOKEN: z.string(),
+  MAPBOX_TOKEN: z.string().optional(),
 });
 
 export const serverEnv = envSchema.parse(process.env);
